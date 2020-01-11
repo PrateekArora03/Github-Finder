82dcc2f08f170f6d26f9392deead3ca63664cb4b
prateekcodes <prateekisthebest98@gmail.com>
2020-01-11 23:34:45 +0530

    add: user to show single user profile

---
+++ src/components/users/User.js
@@ -0,0 +1,66 @@
+import React, { Component, Fragment } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+import Spinner from "../layout/Spinner";
+
+export class User extends Component {
+  async componentDidMount() {
+    await this.props.getUser(this.props.match.params.login);
+  }
+
+  static propTypes = {
+    loading: PropTypes.bool,
+    user: PropTypes.object.isRequired,
+    getUser: PropTypes.func.isRequired
+  };
+
+  render() {
+    const {
+      name,
+      avatar_url,
+      location,
+      bio,
+      blog,
+      login,
+      html_url,
+      followers,
+      following,
+      public_repo,
+      public_gists,
+      hireable
+    } = this.props.user;
+
+    const { loading } = this.props;
+
+    if (loading) return <Spinner />;
+
+    return (
+      <Fragment>
+        <Link to="/" className="btn btn-light">
+          Back to Search
+        </Link>
+        Hireable:{" "}
+        {hireable ? (
+          <i className="fas fa-check text-success" />
+        ) : (
+          <i className="fas fa-times-circle text-danger" />
+        )}
+        <div className="card grid-2">
+          <div className="all-center">
+            <img
+              src={avatar_url}
+              className="round-img"
+              alt=""
+              style={{ width: "150px" }}
+            />
+            <h1>{name}</h1>
+            <p>Location: {location}</p>
+          </div>
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+export default User;
