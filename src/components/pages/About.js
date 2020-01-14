abc668baaae42781e84caaa100a9d0f15c627307
prateekcodes <prateekisthebest98@gmail.com>
2020-01-14 20:53:26 +0530

    update: author date into about.js

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,10 +1,22 @@
 import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
 
 const About = () => {
   return (
     <Fragment>
       <h1>About this App</h1>
       <p>App to search Github users</p>
+      <p>Created By: Prateek Arora</p>
+      <div>
+        <Link to="/user/prateekcodes">Github</Link>{" "}
+        <a
+          href="https://twitter.com/Prateek_Codes"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Twitter
+        </a>
+      </div>
     </Fragment>
   );
 };
