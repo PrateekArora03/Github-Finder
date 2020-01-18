9c0d19d013ee8e1bab9a60b7836a11d91aea1a4c
prateekcodes <prateekisthebest98@gmail.com>
2020-01-18 22:02:25 +0530

    add: Not Found component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Navbar from "./components/layout/Navbar";
 import User from "./components/users/User";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
+import NotFound from "./components/pages/NotFound";
 import Alert from "./components/layout/Alert";
 
 //import state providers
@@ -27,6 +28,7 @@ const App = () => {
                 <Route exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/user/:login" component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
