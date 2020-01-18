30fdbd50c9230f21ad708896d0a1c3422b3af821
prateekcodes <prateekisthebest98@gmail.com>
2020-01-18 18:22:07 +0530

    update: Move User state to context

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,15 +1,21 @@
-import React, { useEffect, Fragment } from "react";
-import PropTypes from "prop-types";
+import React, { useEffect, Fragment, useContext } from "react";
 import { Link } from "react-router-dom";
+
+import GithubContext from "../../context/github/githubContext";
 
 import Repos from "../repos/Repos";
 import Spinner from "../layout/Spinner";
 
-const User = ({ user, loading, repos, getUser, getUserRepos, match }) => {
+const User = ({ match }) => {
+  const githubContext = useContext(GithubContext);
+
+  const { user, repos, loading, getUser, getUserRepos } = githubContext;
+
   useEffect(() => {
     getUser(match.params.login);
     getUserRepos(match.params.login);
-  }, [getUser, getUserRepos, match.params.login]);
+    // eslint-disable-next-line
+  }, []);
 
   const {
     name,
@@ -103,14 +109,6 @@ const User = ({ user, loading, repos, getUser, getUserRepos, match }) => {
       <Repos repos={repos} />
     </Fragment>
   );
-};
-
-User.propTypes = {
-  loading: PropTypes.bool,
-  user: PropTypes.object.isRequired,
-  repos: PropTypes.array.isRequired,
-  getUser: PropTypes.func.isRequired,
-  getUserRepos: PropTypes.func.isRequired
 };
 
 export default User;
--- src/context/github/githubState.js
+++ src/context/github/githubState.js
@@ -34,6 +34,13 @@ const GithubState = props => {
   };
 
   //Get User
+  const getUser = async username => {
+    dispatch({ type: SET_LOADING });
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    dispatch({ type: GET_USER, payload: res.data });
+  };
 
   //Get user repos
   const getUserRepos = async username => {
@@ -62,6 +69,7 @@ const GithubState = props => {
         loading: state.loading,
         searchUsers,
         clearUsers,
+        getUser,
         getUserRepos
       }}
     >
